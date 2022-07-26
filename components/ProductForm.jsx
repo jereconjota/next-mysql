1b02e20333421534b9cc44774fd9a6ea81ff0658
jereconjota <vegeta@jereconjota-mbp.local>
2022-07-26 15:02:51 -0300

    add product useState

--- components/ProductForm.jsx
+++ components/ProductForm.jsx
@@ -1,31 +1,42 @@
 import axios from "axios";
-
+import { useState } from "react";
 
 export function ProductForm() {
 
+    const [product, setProduct] = useState({
+        name: "",
+        price: 0,
+        description: ""
+    });
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const res = await axios.post("/api/products", {
-            name: 'e.target.name.value',
-            price: '23',
-            description: 'e.target.description.value',
-        })
+        const res = await axios.post("/api/products", product);
         console.log(res);
     }
 
+    const handleChange = ({target: {name, value}}) => {
+        setProduct({
+            ...product,
+            [name]: value
+        });
+        console.log(name, value);
+    }
+
+
     return (
-        <div className="bg-gray-300">
-            <form onSubmit={handleSubmit}>
+        <div className="w-full max-w-xs">
+            <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 py-8 mb-4">
                 <label htmlFor="name">Name: </label>
-                <input type="text" name="name" />
+                <input type="text" name="name" onChange={handleChange} className="shadow border rounded py-2 px-3 text-gray-700"/>
 
                 <label htmlFor="price">Price: </label>
-                <input type="text" name="price" id="price" />
+                <input type="text" name="price" id="price" onChange={handleChange} className="shadow border rounded py-2 px-3 text-gray-700"/>
 
                 <label htmlFor="description">Description: </label>
-                <textarea name="description" rows="2"></textarea>
+                <textarea name="description" rows="2" onChange={handleChange} className="shadow border rounded py-2 px-3 text-gray-700"></textarea>
 
-                <button>Save product</button>
+                <button className="bg-purple-500 hover:bg-purple-700 py-2 px-4 rounded focus:outline-none focus:shadow-outline font-bold text-white">Save product</button>
             </form>
         </div>
     )
